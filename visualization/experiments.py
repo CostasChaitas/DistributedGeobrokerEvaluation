882baf2597511fe4885ad1f055ac6d609007a270
CostasChaitas <xaitakon@gmail.com>
2019-12-04 18:56:49 +0100

    Fixing visualization

--- visualization/experiments.py
+++ visualization/experiments.py
@@ -3,10 +3,13 @@ import pandas as pd
 import seaborn as sns
 sns.set(style="whitegrid")
 
-df = pd.read_csv('./data/experiments.csv', sep=';')
-
-#plot = sns.lmplot(x="Nodes", y="Clients", data=df, ci=None, scatter_kws={"s": 50})
+df = pd.read_csv('./data/experiments-clients.csv', sep=';')
 
 plot = sns.lineplot(x="Nodes", y="Clients", data=df, marker='o')
 
+plot.set(xticks=df.Nodes.values)
+
+plot.get_figure().savefig("./output/evaluation-clients.png")
+
 plt.show()
+
