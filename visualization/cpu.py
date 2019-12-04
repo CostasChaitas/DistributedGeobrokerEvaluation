72e7146f651923bf950bf2d1293abd04b6b03052
CostasChaitas <xaitakon@gmail.com>
2019-12-04 19:01:13 +0100

    Fixing visualizations

--- visualization/cpu.py
+++ visualization/cpu.py
@@ -5,6 +5,8 @@ sns.set(style="whitegrid")
 
 df = pd.read_csv('./data/cpu.csv', sep=';')
 
-plot = sns.lineplot(x="Time(s)", y="CPU(%)", hue="Clients",  legend="full", data=df)
+plot = sns.lineplot(x="Time (s)", y="CPU (%)", hue="Experiments",  legend="full", data=df)
+
+plot.get_figure().savefig("./output/cpu.png")
 
 plt.show()
--- visualization/latency.py
+++ visualization/latency.py
@@ -4,19 +4,35 @@ import numpy as np
 import seaborn as sns
 sns.set(style="whitegrid")
 
-df = pd.read_csv('./data/latency.csv', sep=';')
-
-plot = sns.boxplot(x="Experiment", y="Time(ms)", data=df)
-
-means = df.groupby(['Experiment'])['Time(ms)'].mean().values
-median_labels = [str(np.round(s, 2)) for s in means]
-
-pos = range(len(means))
-for tick,label in zip(pos,plot.get_xticklabels()):
-    plot.text(pos[tick], means[tick] + 0.5, median_labels[tick],
-            horizontalalignment='center', size='x-small', color='w', weight='semibold')
+def ecdf(data):
+    """ Compute ECDF """
+    x = np.sort(data)
+    n = x.size
+    y = np.arange(1, n+1) / n * 100
+    return(x,y)
 
 
-plot.set(ylim=(0, 100))
+df1 = pd.read_csv('./data/latency/1node.csv', sep=';')
+df3 = pd.read_csv('./data/latency/3nodes.csv', sep=';')
+df5 = pd.read_csv('./data/latency/5nodes.csv', sep=';')
+
+
+x1,y1 = ecdf(df1["Time(ms)"])
+x3,y3 = ecdf(df3["Time(ms)"])
+x5,y5 = ecdf(df5["Time(ms)"])
+
+
+plot = sns.lineplot(x=x1, y=y1, hue=df1["Experiment"], legend="full", palette=['blue'])
+plot = sns.lineplot(x=x3, y=y3, hue=df3["Experiment"], legend="full", palette=['orange'])
+plot = sns.lineplot(x=x5, y=y5, hue=df5["Experiment"], legend="full", palette=['green'])
+
+
+plot.set(xlabel='Latency (ms)')
+plot.set(xlim=(0, 300))
+y_value=['{:,.2f}'.format(x) + '%' for x in plot.get_yticks()]
+plot.set_yticklabels(y_value)
+plot.set(ylabel='ecdf (x)')
+
+plot.get_figure().savefig("./output/latency.png", bbox_inches = "tight")
 
 plt.show()
